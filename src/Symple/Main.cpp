234feed011e4a8848b2154bbe2380c7622462ddd
Treidex <treidexsemextetry@gmail.com>
2021-02-16 11:25:56 -0800

    cleanup

--- src/Symple/Main.cpp
+++ src/Symple/Main.cpp
@@ -46,7 +46,6 @@ int main(void)
 		return 2;
 
 	puts((const char*)glGetString(GL_VERSION));
-	glClearColor(.25f, .25f, .25f, 1.0f);
 
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
@@ -56,49 +55,25 @@ int main(void)
 
 	auto& io = ImGui::GetIO();
 	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-	io.ConfigFlags |= ImGuiWindowFlags_NoBackground;
 
-	f32 vertices[] = {
-		-.5f,  .5f,
-		 .5f,  .5f,
-		 .5f, -.5f,
-		-.5f, -.5f,
-	};
-
-	u8 indices[] = {
-		0, 1, 2,
-		0, 2, 3,
-	};
-
-	u32 vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(f32), vertices, GL_STATIC_DRAW);
-
-	u32 ibo;
-	glGenBuffers(1, &ibo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, 2 * 3 * sizeof(u32), indices, GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), null);
-
-	bool textEditorPOpen = true;
 	while (!glfwWindowShouldClose(window))
 	{
-		glClear(GL_COLOR_BUFFER_BIT);
-		glDrawElements(GL_TRIANGLES, 4 * 2, GL_UNSIGNED_BYTE, null);
-
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 		ImGui::DockSpaceOverViewport();
 
-		if (textEditorPOpen && ImGui::Begin("Text Editor", &textEditorPOpen))
+		if (ImGui::Begin("Text Editor"))
 		{
 			ImGui::Text("This is your code");
-			ImGui::End();
 		}
+		ImGui::End();
+
+		if (ImGui::Begin("Test"))
+		{
+			ImGui::Text("Test");
+		}
+		ImGui::End();
 
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
